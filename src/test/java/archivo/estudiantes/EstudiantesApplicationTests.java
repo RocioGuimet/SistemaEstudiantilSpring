b6183276bf1bedd781f48566cc0a8e6e90c96eb7
unknown <rocioguimet@hotmail.com>
2025-11-17 18:06:23 -0300

    Estructura

---
+++ src/test/java/archivo/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package archivo.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
