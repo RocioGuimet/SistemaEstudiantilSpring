e37d9d9f4a41f72f11beb6a977ebf3807b3d42ff
unknown <rocioguimet@hotmail.com>
2025-11-25 18:48:55 -0300

    Implementación capa service

--- src/main/java/archivo/estudiantes/service/EstudianteService.java
+++ src/main/java/archivo/estudiantes/service/EstudianteService.java
@@ -1,4 +1,37 @@
 package archivo.estudiantes.service;
 
+import archivo.estudiantes.model.Estudiante;
+import archivo.estudiantes.repository.EstudianteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EstudianteService implements IEstudianteService{
+
+    @Autowired
+    private EstudianteRepository estudianteRepository;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepository.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante findEstudianteById(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepository.findById(idEstudiante). orElse(null);
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepository.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepository.delete(estudiante);
+    }
 }
