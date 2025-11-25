4a0cd61a4f1ea3609e116a0df97cfd5eafca945c
unknown <rocioguimet@hotmail.com>
2025-11-25 19:06:15 -0300

    Configuración capa presentación

--- src/main/java/archivo/estudiantes/EstudiantesApplication.java
+++ src/main/java/archivo/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,34 @@
 package archivo.estudiantes;
 
+import archivo.estudiantes.service.EstudianteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner { //Consola
 
-	public static void main(String[] args) {
-		SpringApplication.run(EstudiantesApplication.class, args);
-	}
+    @Autowired
+    private EstudianteService estudianteService;
+
+    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+    String nl = System.lineSeparator();
+
+    public static void main(String[] args) {
+        logger.info("Iniciando la aplicación...");
+        //Levantar Spring
+        SpringApplication.run(EstudiantesApplication.class, args);
+        logger.info("¡Aplicación finalizada!");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info(nl + "Ejecutando método run" + nl); //Probando funcionamiento
+
+    }
 
 }
